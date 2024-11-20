b51ccaf12373e6c4ef1a5225a5c87de8e2875784
Kamden <birkkamden@gmail.com>
2024-11-19 21:57:54 -0600

    first

---
+++ script.js
@@ -0,0 +1,39 @@
+let entryList =  [];
+const entryInputList = document.getElementById("entries");
+const addEntryButton = document.getElementById("button");
+
+const brick = document.getElementById("brick");
+
+addEntryButton.onclick = add;
+
+function add() {
+    const inputBox = document.createElement("input");
+    document.getElementById("entries").insertBefore(inputBox,brick)
+}
+
+
+const box1 = document.getElementById("1");
+const box2 = document.getElementById("2");
+const box3 = document.getElementById("3");
+const box4 = document.getElementById("4");
+const box5 = document.getElementById("5");
+const box6 = document.getElementById("6");
+const box7 = document.getElementById("7");
+const box8 = document.getElementById("8");
+const box9 = document.getElementById("9");
+const box10 = document.getElementById("10");
+const box11 = document.getElementById("11");
+const box12 = document.getElementById("12");
+const box14 = document.getElementById("14");
+const box15 = document.getElementById("15");
+const box16 = document.getElementById("16");
+const box17 = document.getElementById("17");
+const box18 = document.getElementById("18");
+const box19 = document.getElementById("19");
+const box20 = document.getElementById("20");
+const box21 = document.getElementById("21");
+const box22 = document.getElementById("22");
+const box23 = document.getElementById("23");
+const box24 = document.getElementById("24");
+const box25 = document.getElementById("25");
+
